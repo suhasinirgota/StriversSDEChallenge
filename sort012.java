431c7ddcb4b2324b10d9d759477edeb8b713c396
Suhasini R Gota <suhasinirgota@gmail.com>
2026-06-01 15:51:11 +0530

    Added Dutch National Flag sort012 solution

---
+++ sort012.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortZeroOneTwo(int[] nums) {
+        int low=0, mid=0, high=nums.length -1;//total capacity allocated at creation not filled.
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
+
+    }
+    
+}
+class Main{
+    public static void main(String [] args){
+        int nums[]={1,1,2,2,1};
+        Solution s=new Solution();
+        s.sortZeroOneTwo(nums);
+        for(int num:nums){
+System.out.println(num);
+        }
+    }
+}
